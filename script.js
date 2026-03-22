f34fff6373c7414aa81ae65c855dd35e9dd3a026
Sudo-Spas <spasmission18@gmail.com>
2026-03-22 21:39:16 +0000

    Hopefully it s finally done added js fuck my chungus life

--- script.js
+++ script.js
@@ -1,15 +1,76 @@
 /**
  * @author Andreea-Daniela Borodi
  */
-//let timer=timerLeft;
-let timerLeft=60;
+
+let timeLeft = 60;
+let timer = null;
 let paused = false;
-const timer = document.getElementById("timer");
-function start(){
-   while(timerLeft!=0){
-setInterval( document.getElementById("timer").textContent = '00:'+ timerLeft,1000);
-timerLeft--;
-}
+
+const timerDisplay = document.getElementById("timer");
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
+const stopBtn = document.getElementById("stop");
+
+function formatTime(seconds) {
+    let mins = Math.floor(seconds / 60);
+    let secs = seconds % 60;
+    return mins + ":" + secs.toString().padStart(2, "0");
 }
 
-//alert("Take a break");
+timerDisplay.textContent = formatTime(timeLeft);
+
+function start() {
+
+    timer = setInterval(function () {
+
+        if (!paused) {
+
+            timeLeft--;
+            timerDisplay.textContent = formatTime(timeLeft);
+
+            if (timeLeft < 15) {
+                timerDisplay.classList.add("warning");
+            }
+
+            if (timeLeft <= 0) {
+                clearInterval(timer);
+                alert("Timer is over. Take a break!");
+            }
+
+        }
+
+    }, 1000);
+
+    startBtn.disabled = true;
+    pauseBtn.disabled = false;
+    stopBtn.disabled = false;
+}
+
+function pause() {
+
+    if (paused === false) {
+        paused = true;
+        pauseBtn.textContent = "Resume timer";
+    } else {
+        paused = false;
+        pauseBtn.textContent = "Pause timer";
+    }
+
+}
+
+function stop() {
+
+    clearInterval(timer);
+
+    timeLeft = 60;
+    timerDisplay.textContent = formatTime(timeLeft);
+
+    timerDisplay.classList.remove("warning");
+
+    paused = false;
+    pauseBtn.textContent = "Pause";
+
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
+    stopBtn.disabled = true;
+}
